3a7c6de5d5feded8ac0165c22bf24fa3106f5ede
shevchVit <45031357+shevchVit@users.noreply.github.com>
2019-01-22 19:03:09 +1000

    Распаковка поворота манипулятора

--- UDPConnection.cpp
+++ UDPConnection.cpp
@@ -55,10 +55,13 @@ bool UDPConnection::parsePayload(InputPacket& packet, RovData& rov_data)
 
 	// кнопки 4 5 6 не трогать	
 
+
+	///////////Axis///////////
 	rov_data.m_axis_x = packet.axisX_p;
 	rov_data.m_axis_y = packet.axisY_p;
 	rov_data.m_axis_z = packet.axisW_p;
 	rov_data.m_axis_w = packet.axisZ_p;
+	//////////////////////////
 
 	///////////Cameras///////////
 	switch (packet.camera_rotate)
@@ -83,55 +86,14 @@ bool UDPConnection::parsePayload(InputPacket& packet, RovData& rov_data)
 	/////////////////////////////
 
 	///////////Manipulator///////////
-	if (actionState[0] == 1) rov_data.m_manipulator_grab = -1;
-	else if (actionState[1] == 1) rov_data.m_manipulator_grab = 1;
+	if (actionState[0] == 1) rov_data.m_manipulator_grab = 1;
+	else if (actionState[1] == 1) rov_data.m_manipulator_grab = -1;
 	else rov_data.m_manipulator_grab = 0;
+	rov_data.m_manipulator_rotate = packet.manipulator_rotate;
 	/////////////////////////////////
 
+	//PRINTROVDATA(1000);
 
-	PRINTROVDATA(1000);
-
-
-	//rov_data.m_rotary_camera_1 = packet.camera_rotate;
-
-	//rov_data.m_rotary_camera_1 = packet.cameraRotation[0];
-	//rov_data.m_rotary_camera_2 = packet.cameraRotation[1];
-
-	//rov_data.m_regulator_type = packet.regulators;
-
-
-
-	//rov_data.m_manipulator_grab = packet.manipulatorGrab;
-
-	//rov_data.m_manipulator_rotate = packet.manipulator_rotate;
-	
-	/*if (m_timer_.elapsed() > 1000)
-	{
-		for (int i = 0; i < 12; i++)
-		{
-			Serial.print("button = ");
-			Serial.print(i);
-			Serial.print("     value = ");
-			Serial.println(actionState[i]);
-		}
-		Serial.print("m_axis_x = ");
-		Serial.println(rov_data.m_axis_x);
-		Serial.print("m_axis_y = ");
-		Serial.println(rov_data.m_axis_y);
-		Serial.print("m_axis_z = ");
-		Serial.println(rov_data.m_axis_z);
-		Serial.print("m_axis_w = ");
-		Serial.println(rov_data.m_axis_w);
-		Serial.print("m_rotary_camera_1 = ");
-		Serial.println(rov_data.m_rotary_camera[0]);
-		Serial.print("m_rotary_camera_2 = ");
-		Serial.println(rov_data.m_rotary_camera[1]);
-		Serial.print("m_manipulator_rotate = ");
-		Serial.println(rov_data.m_manipulator_rotate);
-		Serial.print("m_manipulator_grab = ");
-		Serial.println(rov_data.m_manipulator_grab);
-		m_timer_.start();
-	}*/
 	return true;
 }
 
