cf613b090a1d03768c9f94347d0e7cc374fac35c
shevchVit <45031357+shevchVit@users.noreply.github.com>
2019-02-01 16:52:17 +1000

    Update ROV_MACROS.h

--- ROV_MACROS.h
+++ ROV_MACROS.h
@@ -19,16 +19,16 @@
 #define PRINTDEBUG(str)
 #endif
 
-#ifdef SUBSYSTEMSTEST			
-#define SERIALENABLE			
-#define SUBSYSTEMPRINT(str)		\
-	Serial.print("FILE: ");		\
-	Serial.print(__FILE__);		\
-	Serial.print(" | LINE: ");	\
-	Serial.print(__LINE__);		\
-	Serial.print(" | MSG: ");	\
-	Serial.println(str);		\
-
-#else						
-#define SUBSYSTEMPRINT(str)			
-#endif
+//#ifdef SUBSYSTEMSTEST			
+//#define SERIALENABLE			
+//#define SUBSYSTEMPRINT(str)		\
+//	Serial.print("FILE: ");		\
+//	Serial.print(__FILE__);		\
+//	Serial.print(" | LINE: ");	\
+//	Serial.print(__LINE__);		\
+//	Serial.print(" | MSG: ");	\
+//	Serial.println(str);		\
+//
+//#else						
+//#define SUBSYSTEMPRINT(str)			
+//#endif
