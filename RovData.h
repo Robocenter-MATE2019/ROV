d387bbe305264a2078b01d8d84078c1626aad5a1
shevchVit <45031357+shevchVit@users.noreply.github.com>
2019-02-26 19:23:28 +1000

    Добавлены новые поля

--- RovData.h
+++ RovData.h
@@ -15,11 +15,13 @@ struct RovData
 	int8_t m_manipulator_grab;
 	int8_t m_manipulator_rotate;
 	int8_t m_rotary_camera[2] = { 0, 0 };
-	//int8_t m_rotary_camera_1;
-	//int8_t m_rotary_camera_2;
 	int8_t m_yaw;
 	int8_t m_roll;
 	int8_t m_pitch;
 	int8_t m_depth;
+	int8_t m_yaw_to_set;
+	int8_t m_roll_to_set;
+	int8_t m_pitch_to_set;
+	int8_t m_depth_to_set;
 	int8_t	m_temperature;
 };
